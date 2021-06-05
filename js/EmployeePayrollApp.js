abaa0400aca42a8fa2faab0a807859413813fcd9
ok97 <omkhawshi0@gmail.com>
2021-06-06 01:01:57 +0530

    Added: Reset the form on clicking reset

--- js/EmployeePayrollApp.js
+++ js/EmployeePayrollApp.js
@@ -118,3 +118,26 @@ function createAndUpdateStorage(employeePayrollData)
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
 }
 
+
+
+/* UC5:- Ability to reset the form on clicking reset  */
+const resetForm = () => {
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', ' ');
+    setValue('#notes', ' ');
+    setValue('#day', '1');
+    setValue('#month', 'Jan');
+    setValue('#year', '2020');
+}
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => { item.checked = false; }
+    );
+}
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
